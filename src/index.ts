84f30f59fa6cd3d9ed546543ea03d5f67e5093f7
artleitch-gyg <hello@artleitch.com>
2020-11-09 18:01:25 +1030

    Split up module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 require('source-map-support').install()
+const { module01 } = require('./modules/module-01/module-01')
 
 exports.jsGym = {
   algorithmOne(a: number, b: number) {
@@ -8,4 +9,7 @@ exports.jsGym = {
   algorithmTwo(a: number, b: number) {
     return a * b
   },
+
+  module01,
 }
+export {}
--- src/modules/module-01/test/test.ts
+++ src/modules/module-01/test/test.ts
@@ -1,16 +1,17 @@
 const assert = require('assert')
-const { jsGym } = require('../index')
 
-describe('jsGym', function () {
-  describe('#indexOf()', function () {
+const { module01 } = require('../module-01')
+describe('Module 01', function () {
+  describe('algorithm', function () {
     it('should return -1 when the value is not present', function () {
-      const result = jsGym.algorithmOne(1, 2)
+      const result = module01.algorithmOne(1, 2)
       assert.equal(result, 3)
     })
     it('should return -1 when the value is not present', function () {
-      const result2 = jsGym.algorithmTwo(1, 2)
+      const result2 = module01.algorithmTwo(1, 2)
       assert.equal(result2, 2)
     })
   })
 })
+
 export {}
