a21f9cfc70b14aaac9a425ab85903e8075871e4a
artleitch-gyg <hello@artleitch.com>
2020-11-11 14:18:05 +1030

    Fix use of modules in index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import module02 from './modules/module02/module02'
+import Module02 from './modules/module02/module02'
 import WarmUpChallenges from './modules/warm-up-challenges/warm-up-challenges'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('source-map-support').install()
 
 const jsGym = {
-  warmUpChallenges: WarmUpChallenges,
-  module02,
+  WarmUpChallenges,
+  Module02,
 }
 
 export default jsGym
