79baa073c0c80c1f67a63ada51477944bb3c55f3
artleitch-gyg <hello@artleitch.com>
2020-12-02 23:16:30 +1030

    Add tests for greedy algorithm

--- src/modules/greedy-algorithms/greedy-algorithms.test.ts
+++ src/modules/greedy-algorithms/greedy-algorithms.test.ts
@@ -14,5 +14,41 @@ describe('GreedyAlgorithms', () => {
       expect(GreedyAlgorithms.minimumDiffInArray(arr)).to.equal(1)
     })
   })
+  describe('#candies', () => {
+    it('', () => {
+      const arr = []
+      // expect(GreedyAlgorithms.minimumDiffInArray(arr)).to.equal(3)
+    })
+  })
+  describe('#maximumPerimeterTriangle', () => {
+    it('', () => {
+      const arr = []
+      // expect(GreedyAlgorithms.minimumDiffInArray(arr)).to.equal(3)
+    })
+  })
+  describe('#gridChallenge', () => {
+    it('', () => {
+      const arr = []
+      // expect(GreedyAlgorithms.minimumDiffInArray(arr)).to.equal(3)
+    })
+  })
+  describe('#marcsCakewalk', () => {
+    it('should return the minimum calories', () => {
+      const arr = [7, 4, 9, 6]
+      expect(GreedyAlgorithms.marcsCakewalk(arr)).to.equal(79)
+    })
+  })
+  describe('#greedyFlorist', () => {
+    it('should return the minimum price', () => {
+      const arr = [1, 3, 5, 7, 9]
+      expect(GreedyAlgorithms.greedyFlorist(3, arr)).to.equal(29)
+    })
+  })
+  describe('#markAndToys', () => {
+    it('should return correct number of toys', () => {
+      const arr = [1, 12, 5, 111, 200, 1000, 10]
+      expect(GreedyAlgorithms.markAndToys(arr, 50)).to.equal(4)
+    })
+  })
 })
 export {}
